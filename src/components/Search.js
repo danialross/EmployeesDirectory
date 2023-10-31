a0178e4c4464f32a0907d4b765821728e57bc246
danialross <92746365+danialross@users.noreply.github.com>
2023-10-31 12:12:26 -0400

    search and reset
    added reset and search button. user now requires to press enter or search to get result instead of only typing.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navbar, Form, Button } from "react-bootstrap";
+import { Navbar, Form, CloseButton, Button } from "react-bootstrap";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -15,25 +15,25 @@ const StyledForm = styled(Form.Control)`
   font-family:
     Varela Round,
     sans-serif;
-  width: 18rem;
-  margin-right: 1rem;
+  width: 16rem;
+  margin-right: -2rem;
   display: flex;
-  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const StyledCloseButton = styled(CloseButton)`
+  margin-right: 1rem;
+`;
+
+const StyledButton = styled(Button)`
+  margin-right: 0.5rem;
 `;
 
 function Search({ setter, refreshDatabase }) {
   const [input, setInput] = useState("");
 
-  let timer;
-
   const handleChange = (e) => {
-    clearTimeout(timer);
-    const input = e.target.value;
-    setInput(input);
-
-    timer = setTimeout(() => {
-      searchDatabase(input);
-    }, 300);
+    setInput(e.target.value);
   };
 
   const searchDatabase = (input) => {
@@ -51,32 +51,41 @@ function Search({ setter, refreshDatabase }) {
   };
 
   const handleReset = () => {
-    if (input !== "") {
-      setInput("");
-      refreshDatabase();
-    }
+    handleClear();
+    refreshDatabase();
   };
 
-  const disableEnter = (e) => {
+  const handleClear = () => {
+    setInput("");
+  };
+
+  const enterPressed = (e) => {
     if (e.key === "Enter") {
-      e.preventDefault();
+      searchDatabase(input);
     }
   };
 
   return (
     <StyledNav>
-      <Form>
-        <StyledForm
-          type="text"
-          placeholder="Search by Name or Title"
-          value={input}
-          onChange={handleChange}
-          onKeyDown={disableEnter}
-        />
-      </Form>
-      <Button onClick={handleReset} variant="outline-primary">
-        Clear
-      </Button>{" "}
+      <StyledForm
+        type="text"
+        placeholder="Search by Name or Title"
+        value={input}
+        onChange={handleChange}
+        onKeyDown={enterPressed}
+      />
+      <StyledCloseButton onClick={handleClear} />
+      <StyledButton
+        onClick={() => {
+          searchDatabase(input);
+        }}
+        variant="primary"
+      >
+        Search
+      </StyledButton>
+      <Button onClick={handleReset} variant="secondary">
+        Reset
+      </Button>
     </StyledNav>
   );
 }
